4b81eff567fa0546cd4b50075642c41269333c0e
Rakib9001 <100214638+Rakib9001@users.noreply.github.com>
2024-01-22 22:49:23 +0600

    added new files

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+  var typed = new Typed((".rakibul"),{
+    strings : ["RAKIBUL ISLAM"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+})
